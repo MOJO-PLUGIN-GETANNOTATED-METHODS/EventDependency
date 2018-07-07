e90727993af2070db0acc7a494c6c467da9f2a26
Dev <dev.prabhakaran@walmart.com>
2018-07-07 19:55:26 +0530

    A sample Custom Annotation

---
+++ src/com/src/walmart/Event.java
@@ -0,0 +1,15 @@
+package com.src.walmart;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Event {
+	public int x();
+
+	public int y();
+}
